545cf95f086810e1bcb6350141d2b23fd667740a
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2024-10-03 19:56:56 +0200

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `marketplace` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) DEFAULT NULL,
+  `item` varchar(50) DEFAULT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
